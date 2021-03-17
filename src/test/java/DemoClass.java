1c65936717107772f5533837dc95c31affe963c3
KiranJadhao <kiran.jadhao@ibm.com>
2021-03-17 08:42:53 +0530

    Second commit with screenshot functionality

--- src/test/java/DemoClass.java
+++ src/test/java/DemoClass.java
@@ -1,19 +1,24 @@
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.junit5.ScreenShooterExtension;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class DemoClass {
+
    @Test
     public void goToURL(){
-    open("https://www.imdb.com/");
-    $(By.name("q")).setValue("games of thrones").pressEnter();
-    $(By.xpath("//*[@id=\"main\"]/div/div[2]/table/tbody/tr[4]/td[2]/a")).pressEnter();
-    $("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > h1").shouldHave(Condition.text("Game of Thrones ")).getScreenshotAs(OutputType.FILE);
+       ScreenShooterExtension screenShooterExtension = new ScreenShooterExtension().to("target/selenide");
+       open("https://www.imdb.com/");
+       screenshot("Open_URL");
+       $(By.name("q")).setValue("games of thrones").pressEnter();
+       screenshot("Search_Text");
+       $(By.xpath("//*[@id=\"main\"]/div/div[2]/table/tbody/tr[4]/td[2]/a")).pressEnter();
+       screenshot("Search_Result");
+       $("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > h1").shouldHave(Condition.text("Game of Thrones "));
+       screenshot("Verify_LandingPage");
    }
 }
